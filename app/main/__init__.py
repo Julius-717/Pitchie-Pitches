b88376fc09d0c948ef5107f0a196d7a496126a47
Julius-717 <waynejulius10@gmail.com>
2019-11-27 22:57:59 +0300

    add app configurations

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,3 +2,10 @@ from flask import Blueprint
 main = Blueprint('main',__name__)
 from . import views,error
 
+def create_app(config_name):
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Ini
