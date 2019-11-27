4b9f021cbee6d26e0c7588b1a1c8c6d2870f535c
Julius-717 <waynejulius10@gmail.com>
2019-11-28 02:35:46 +0300

    add flask imports

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Comment,User,Pitch
+# We may also use the import * command to import all objects from a specific module e.g from ..models import *
+# ,get_pitch,get_comments
+from . import main
+from .forms import CommentForm, PitchForm,UpdateProfile
+from flask_login import login_required, current_user
+from .. import db,photos
+import markdown2
