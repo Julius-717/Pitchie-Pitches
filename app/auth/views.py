a859886654e793f06be6f4456bb08e9b3bd6ca96
Julius-717 <waynejulius10@gmail.com>
2019-11-27 22:38:37 +0300

    add flask imports

---
+++ app/auth/views.py
@@ -0,0 +1,10 @@
+# from flask impoer render_template
+from . import auth
+from flask import render_template,redirect,url_for, flash,request
+from ..models import User
+from flask_login import login_user,logout_user,login_required
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from ..email import mail_message
+
+@auth.route()
