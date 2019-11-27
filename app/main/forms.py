ca010543e98855bde0e5ac957d8afa4c4e3963bb
Julius-717 <waynejulius10@gmail.com>
2019-11-28 02:34:03 +0300

    add update profile form

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,7 @@ from wtforms import StringField,TextAreaField,SubmitField,SelectField
 class CommentForm(FlaskForm):
     comment = TextAreaField('Your comment...')
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.')
+    submit = SubmitField('Submit')
