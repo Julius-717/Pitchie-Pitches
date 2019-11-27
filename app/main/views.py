dca5fc286a450d7688e5abc1ed0996313644af39
Julius-717 <waynejulius10@gmail.com>
2019-11-28 02:36:48 +0300

    add save area

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,7 @@ from .forms import CommentForm, PitchForm,UpdateProfile
 from flask_login import login_required, current_user
 from .. import db,photos
 import markdown2
+
+
+def save_pitch(pitch):
+    Pitch.save_pitch(pitch)
